0709230085fd4a6495a6de4ddbd5ff3577d36983
Opyt Rafael <rafael.julio.dev@outlook.com>
2025-09-18 17:55:05 -0300

    Revining profit calculator
    - Added error handling
    - Writing file with calculation results

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -1,30 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+const outputFileName = "calculations.txt"
 
 func main() {
 
 	var revenue float64
 	var expenses float64
 	var taxRate float64
+	var err error
 
-	revenue = getUserInput("Enter Revenue: ")
+	revenue, err = getUserInput("Enter Revenue: ")
+	if err != nil {
+		panic(err)
+	}
 
-	expenses = getUserInput("Enter expenses: ")
+	expenses, err = getUserInput("Enter expenses: ")
+	if err != nil {
+		panic(err)
+	}
 
-	taxRate = getUserInput("Enter Tax Rate: ")
+	taxRate, err = getUserInput("Enter Tax Rate: ")
+	if err != nil {
+		panic(err)
+	}
 
 	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
 
 	fullMessage := createOutputMessage(ebt, profit, ratio)
 
+	writeCalculationsInFile(fullMessage)
+
 	fmt.Println(fullMessage)
 }
 
-func getUserInput(message string) (userInput float64) {
+func getUserInput(message string) (userInput float64, err error) {
 	fmt.Print(message)
 	fmt.Scan(&userInput)
-	return userInput
+
+	if userInput < 0 {
+		err := errors.New("input must be a positive number")
+		return 0, err
+	}
+
+	if userInput == 0 {
+		err := errors.New("input cannot be 0")
+		return 0, err
+	}
+
+	return userInput, nil
 }
 
 func calculate(revenue float64, expenses float64, taxRate float64) (ebt float64, profit float64, ratio float64) {
@@ -32,6 +61,11 @@ func calculate(revenue float64, expenses float64, taxRate float64) (ebt float64,
 	profit = ebt * (1 - taxRate)
 	ratio = ebt / profit
 	return ebt, profit, ratio
+}
+
+func writeCalculationsInFile(output string) {
+	outputBytes := []byte(output)
+	os.WriteFile(outputFileName, outputBytes, 0644)
 }
 
 func createOutputMessage(ebt float64, profit float64, ratio float64) (message string) {
